baa3f23c95ff5f95341ec31d230954275ce6a08f
Angelica Alcalá <143430188+ara589@users.noreply.github.com>
2023-11-30 15:07:47 -0700

    Update FlagMaxTemp.R

--- FlagMaxTemp.R
+++ FlagMaxTemp.R
@@ -1,11 +1,1 @@
-#' Flagstaff Maximum Temperatures
-#'
-#' This dataset contains maximum daily temperatures in Flagstaff, AZ
-#' for the years 1985 - 2015.
-#'
-#' @format A data frame with 10882 observations with 2 columns.
-#' \describe{
-#'    \item{Date}{this is the date of the observation in year, month, day format}
-#'    \item{MaxTemp}{This is the temperature measurement in degrees Fahrenheit}
-#' }
-"FlagMaxTemp"
+
